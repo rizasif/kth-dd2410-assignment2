409860ab774464d02cd63a720cf0570580f7b463
rizasif <rizasif@gmail.com>
2018-09-14 10:54:45 +0200

    pulling

--- kinematics_assignment/scripts/IK_functions.py
+++ kinematics_assignment/scripts/IK_functions.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
+import math
+
 """
-    # {student full name}
-    # {student email}
+    # Rizwan Asif
+    # rasif@kth.se
 """
 
 def scara_IK(point):
@@ -10,11 +12,26 @@ def scara_IK(point):
     y = point[1]
     z = point[2]
     q = [0.0, 0.0, 0.0]
+    
+    L0 = 0.07    
+    L1 = 0.3
+    L2 = 0.35
 
-    """
-    Fill in your IK solution here and return the three joint values in q
-    """
-
+    #subtracting offset
+    x -= L0
+    
+    #t2 calculation
+    t2 = math.acos( (math.pow(x,2) + math.pow(y,2) - math.pow(L1,2) - math.pow(L2,2))/(2*L1*L2) )
+    
+    #t1 calculation
+    num = y*(L2*math.cos(t2)+L1) - x*(L2*math.sin(t2))
+    dnum = x*(L2*math.cos(t2)+L1) + y*(L2*math.sin(t2)) 
+    t1 = math.atan2(num,dnum)
+    
+    q = [t1, t2, z]
+    
+    print("Returning {}".format(q))
+    
     return q
 
 def kuka_IK(point, R, joint_positions):
